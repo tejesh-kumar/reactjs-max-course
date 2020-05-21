d564c43fa1ecdf180f9f97f8e4de06f787dd97d0
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-21 15:48:19 +0530

    Output dynamic content in jsx using curly braces     const person = () => {         return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>     }
    Advantages of components,
        Distribute the code to various files and importing helps to maintain.
        Components are also reusable.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <h1>Hi, from React app</h1>
         <p>This is really working</p>
         <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement('div', { className: App }, React.createElement('h1', null, 'Hi, from React app'))
