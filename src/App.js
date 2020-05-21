ea69154c9b875a927cd477efaaa12c2b19bc5f0b
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-21 15:10:39 +0530

    Understanding JSX Jsx syntax what we write similar to html,      return (        <div className="App">          <h1>Hi, from React app</h1>        </div>      );
    Transformed to js as shown below behind the scenes,
        return React.createElement('div', { className: App }, React.createElement('h1', null, 'Hi, from React app'))

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, from React app</h1>
-      </div>
-    );
+    // return (
+      // <div className="App">
+      //   <h1>Hi, from React app</h1>
+      // </div>
+    // );
+    return React.createElement('div', { className: App }, React.createElement('h1', null, 'Hi, from React app'))
   }
 }
 
