a7a2d2750e626904cc392f6743ab302e5b8db6d6
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-21 18:34:14 +0530

    useState() hook for state management Managing state using setState() available only in class-based components. From React 16.8, we can manage state in functional components using react hooks. useState() is react-hook to manage state in functional components. usestate() returns an array with 2 elements, 1st element is current state, 2nd element is a function that allows us to update the state, such that react is aware of it & will re-render this component. The difference between changing state in class components using 'setState()' & 'useState()' hook in functional components is 'setState' merges the old state with new state(keeps the other properties of the state unchanged if it was existing) where as in 'useState()' hook replaces the entire old state object to new state object. Hence, the updatedState we must manually include all the old state data, so other properties like ('someOtherState' property of state) to not be deleted. Instead of manually merging the state, in functional components, we can use multiple 'useState()' calls
    Refer to app.js file to compare the class & functional components managing state in different ways.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: "Tejesh", age: 25 },
-      { name: "Nitesh", age: 23 },
-      { name: "Madhu", age: 26 }
-    ],
-    otherState: 'some other value'
-  }
+// class App extends Component {
+//   state = {
+//     persons: [
+//       { name: "Tejesh", age: 25 },
+//       { name: "Nitesh", age: 23 },
+//       { name: "Madhu", age: 26 }
+//     ],
+//     otherState: 'some other value'
+//   }
 
-  switchNameHandler = () => {
-    this.setState({
+//   switchNameHandler = () => {
+//     this.setState({
+//         persons: [
+//           { name: "Tejesh Kumar", age: 25 },
+//           { name: "Nitesh", age: 23 },
+//           { name: "Madhu", age: 27 }
+//         ]
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi, from React app</h1>
+//         <p>This is really working</p>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+
+//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+
+const app = () => {
+
+  const [ personsState, setPersonsState ] = useState({
+                      persons: [
+                        { name: "Tejesh", age: 25 },
+                        { name: "Nitesh", age: 23 },
+                        { name: "Madhu", age: 26 }
+                      ]
+                    })
+
+                                          
+
+  const [ otherState, setOtherState ] = useState({ otherState: 'some other value' })
+
+  console.log(personsState, otherState);
+
+
+  const switchNameHandler = () => {
+    setPersonsState({
         persons: [
           { name: "Tejesh Kumar", age: 25 },
           { name: "Nitesh", age: 23 },
@@ -22,41 +70,24 @@ class App extends Component {
     })
   }
 
-  render() {
     return (
       <div className="App">
         <h1>Hi, from React app</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={switchNameHandler}>Switch Name</button>
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+
+
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
 
 
 
-// switchNameHandler - 'Handler' part of method name is a convention. This is a method we are not calling but active assigned to handle events.
-// Methods & properties of class components are used in es6 syntax.
-// Do not mutate the state directly, this.state.persons[0].name = 'Tejesh Kumar';
 
 
-// Component Object has a setState() method is used to update the state. This method takes an object as argument & merges to the existing state.
-
-  // switchNameHandler = () => {
-  //   this.setState({
-  //       persons: [
-  //         { name: "Tejesh Kumar", age: 25 },
-  //         { name: "Nitesh", age: 23 },
-  //         { name: "Madhu", age: 27 }
-  //       ]
-  //   })
-  // }
-// setState only updates the 'persons' array values which are changed but 'otherState' property is not changed in the 'state' & is not discarded either.
-
-// Hence, changes in 'state' or 'props' leads React to re-render the DOM.
