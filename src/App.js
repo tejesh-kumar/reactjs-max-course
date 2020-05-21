c9a9e720c4f3cb937c1b8b3568191f83273aa6ea
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-21 16:36:40 +0530

    Understanding state Sometimes, we don't want paremeters to be passed from outside, but some variables within the component & change it from inside 'state' is a special property that works only in class-based components. 'state' is a special property because as once the state property values change, it will lead react to re-render the DOM. 'props' are set & passed from outside where as, the 'state' is set & managed inside the component. Functional components should be as maximum components & class-based state managing components as minimum as possible for maintainability. From react 16.6, 'state' can be managed even in functional components using react hooks.
    In App.js,
        class App extends Component {
        state = {
            persons: [
            { name: "Tejesh", age: 25 },
            { name: "Nitesh", age: 23 },
            { name: "Madhu", age: 26 }
            ]
        }
    
        render() {
            return (
            <div className="App">
                <h1>Hi, from React app</h1>
                <p>This is really working</p>
                <button>Switch Name</button>
    
                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
            </div>
            );
        }
        }

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Tejesh", age: 25 },
+      { name: "Nitesh", age: 23 },
+      { name: "Madhu", age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, from React app</h1>
         <p>This is really working</p>
-        <Person name="Tejesh" age="25"></Person>
-        <Person name="Nitesh" age="23">My hobbies: Racing</Person>
-        <Person name="Madhu" age="26"></Person>
+        <button>Switch Name</button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
   }
