d08620aed0144df9b0b9163ea3d2740ac52aec77
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-22 23:00:42 +0530

    Outputting Lists Using map(), maps each person object to a valid JSX Person component to be rendered, based on state.     class App extends Component {     state = {         persons: [         { name: "Tejesh", age: 25 },         { name: "Nitesh", age: 23 },         { name: "Madhu", age: 26 }         ],         otherState: 'some other value',         showPersons: false     }
        switchNameHandler = (newName) => {
            this.setState({
                persons: [
                { name: newName, age: 25 },
                { name: "Nitesh", age: 23 },
                { name: "Madhu", age: 27 }
                ]
            })
        }
    
        nameChangedHandler = (event) => {
            this.setState({
            persons: [
                { name: "Tejesh", age: 25 },
                { name: event.target.value, age: 23 },
                { name: "Madhu", age: 27 }
            ]
            })
        }
    
        togglePersonsHandler = () => {
            const doesShow = this.state.showPersons;
            this.setState({
            showPersons: !doesShow
            })
        }
    
        render() {
            const style = {
            backgroundColor: 'white',
            font: 'inherit',
            border: '1px solid blue',
            padding: '8px',
            cursor: 'pointer'
            }
    
            let persons = null;
            if(this.state.showPersons) {
            persons = (
                <div>
                {
                    this.state.persons.map((person) => {
                    return <Person name={person.name} age={person.age} />
                    })
                }
                </div>
            )
            }
    
            return (
            <div className="App">
                <h1>Hi, from React app</h1>
                <p>This is really working</p>
                <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
                { persons }
            </div>
            );
        }
        }
    
    Presently getting an error "Each child in the list should have unique key prop" to identify each Person component among others.

--- src/App.js
+++ src/App.js
@@ -53,12 +53,18 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+          {
+            this.state.persons.map((person) => {
+              return <Person name={person.name} age={person.age} />
+            })
+          }
+
+          {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
           <Person name={this.state.persons[1].name} 
           age={this.state.persons[1].age} 
           click={this.switchNameHandler.bind(this, 'Tejesh Kumar B M')} 
           changed={this.nameChangedHandler}>My hobbies: Racing</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person> */}
         </div> 
       )
     }
@@ -78,7 +84,6 @@ export default App;
 
 
 
- 
 
 
 
