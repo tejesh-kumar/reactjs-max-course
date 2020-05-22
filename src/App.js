71b229a255cf3222bc883f9e81f146ffc343c126
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-22 17:51:55 +0530

    Styling using inline styles     <button style={style} onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button> style attribute provided by JSX to add styling to the components. styling the hover effect is very hard using inline styles.
    In App component,
      render() {
        const style = {
          backgroundColor: 'white',
          font: 'inherit',
          border: '1px solid blue',
          padding: '8px',
          cursor: 'pointer'
        }
    
        return (
          <div className="App">
            <h1>Hi, from React app</h1>
            <p>This is really working</p>
            <button style={style} onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button>
    
            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
            <Person name={this.state.persons[1].name}
            age={this.state.persons[1].age}
            click={this.switchNameHandler.bind(this, 'Tejesh Kumar B M')}
            changed={this.nameChangedHandler}>My hobbies: Racing</Person>
            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
          </div>
        );
      }

--- src/App.js
+++ src/App.js
@@ -33,11 +33,19 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, from React app</h1>
         <p>This is really working</p>
-        <button onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button>
 
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} 
