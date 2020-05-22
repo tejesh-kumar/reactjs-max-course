34761f488856ad3eba18a3b40277353f5489b020
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-22 17:23:43 +0530

    Adding 2 way data binding Initial state shows the user info on page load. nameChangedHandler() is called for observing changes in input field, thereby updating the state, the state is again propogated and DOM re-rendered.
    In app.js,
      class App extends Component {
        state = {
          persons: [
            { name: "Tejesh", age: 25 },
            { name: "Nitesh", age: 23 },
            { name: "Madhu", age: 26 }
          ],
          otherState: 'some other value'
        }
    
        switchNameHandler = (newName) => {
          this.setState({
              persons: [
                { name: newName, age: 25 },
                { name: "Nitesh", age: 23 },
                { name: "Madhu", age: 27 }
              ]
          })
        }
    
        nameChangedHandler = (event) => {
          this.setState({
            persons: [
              { name: "Tejesh", age: 25 },
              { name: event.target.value, age: 23 },
              { name: "Madhu", age: 27 }
            ]
          })
        }
    
        render() {
          return (
            <div className="App">
              <h1>Hi, from React app</h1>
              <p>This is really working</p>
              <button onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button>
    
              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
              <Person name={this.state.persons[1].name}
              age={this.state.persons[1].age}
              click={this.switchNameHandler.bind(this, 'Tejesh Kumar B M')}
              changed={this.nameChangedHandler}>My hobbies: Racing</Person>
              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
            </div>
          );
        }
      }
    
    In Person.js,
      const person = (props) => {
        return (
            <div>
                <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
                <p>{props.children}</p>
                <input type="text" onChange={props.changed} value={props.name} />
            </div>
        )
      }
    
      Providing a value to input field without handling the change events, locks the input field, as present unchanged state is constantly over-riding and input field cannot be typed.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Tejesh", age: 25 },
+        { name: event.target.value, age: 23 },
+        { name: "Madhu", age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,7 +40,10 @@ class App extends Component {
         <button onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button>
 
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Tejesh Kumar B M')}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        click={this.switchNameHandler.bind(this, 'Tejesh Kumar B M')} 
+        changed={this.nameChangedHandler}>My hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     );
@@ -41,72 +54,6 @@ export default App;
 
 
 
-// Methods in one component can be passed as props(references) to other components, that function can be called in through props.refFuncName.
-// In app component,
-  // class App extends Component {
-  //   state = {
-  //     persons: [
-  //       { name: "Tejesh", age: 25 },
-  //       { name: "Nitesh", age: 23 },
-  //       { name: "Madhu", age: 26 }
-  //     ],
-  //     otherState: 'some other value'
-  //   }
-
-  //   switchNameHandler = (newName) => {
-  //     this.setState({
-  //         persons: [
-  //           { name: newName, age: 25 },
-  //           { name: "Nitesh", age: 23 },
-  //           { name: "Madhu", age: 27 }
-  //         ]
-  //     })
-  //   }
-
-  //   render() {
-  //     return (
-  //       <div className="App">
-  //         <h1>Hi, from React app</h1>
-  //         <p>This is really working</p>
-  //         <button onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button>
-
-  //         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-
-  //         <Person 
-  //          name={this.state.persons[1].name} 
-  //          age={this.state.persons[1].age} 
-  //          click={this.switchNameHandler.bind(this, 'Tejesh Kumar B M')}>My hobbies: Racing
-  //         </Person>
-
-  //         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
-  //       </div>
-  //     );
-  //   }
-  // }
-  
-
-// In person component,
-  // const person = (props) => {
-  //   return (
-  //       <div>
-  //           <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-  //           <p>{props.children}</p>
-  //       </div>
-  //   )
-  // }
-
-
-
-// Passing parameters to method using bind(this, 'tejesh');
-// <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Tejesh Kumar B M')}>My hobbies: Racing</Person>
-
-// Passing parameters to method using arrow functions, () => this.switchNameHandler('Tejesh Kumar')
-// <button onClick={() => this.switchNameHandler('Tejesh Kumar')}>Switch Name</button>
-// This is a bit inefficient way, as this leads react to re-render certain things too often.
-
-// Stateful & Stateless components
-// Stateful(container) components is a component that manages state (eg:- App component) & stateless(presentational) components are components that don't manage state (eg:- Person Component).
-// Good practice - More stateless components & less stateful components.
 
 
 
