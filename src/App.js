12625b3deb97bd19b1425b24afe84aa6eeb9c0ea
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-23 16:19:41 +0530

    Adding and using Radium Helps to use pseudo-selectors & media queries with inline styles. Can be used for both class & functional components. export default Radium(App);   App is wrapped in Radium()(higher order component).   render() {     const style = {       backgroundColor: 'green',       color: 'white',       font: 'inherit',       border: '1px solid blue',       padding: '8px',       cursor: 'pointer',       ':hover': {         backgroundColor: 'lightgreen',         color: 'black'       }     }
        let persons = null;
        if(this.state.showPersons) {
          persons = (
            <div>
              {
                this.state.persons.map((person, index) => {
                  return <Person
                    id = {index} name={person.name}
                    age = {person.age}
                    click = {() => { this.deletePersonHandler(index)}}
                    changed = {(event) => this.nameChangedHandler(event, person.id)}
                    key = {person.id} />
                })
              }
            </div>
          )
    
          style.backgroundColor = 'red';
          style[':hover'] = {
            backgroundColor: 'salmon',
            color: 'black'
          }
        }
    
        let classes = [];
        if(this.state.persons.length <= 2) {
          classes.push('red');
        }
        if(this.state.persons.length <= 1) {
          classes.push('bold');
        }
    
        return (
          <div className="App">
            <h1>Hi, from React app</h1>
            <p className={classes.join(' ')}>This is really working</p>
            <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
            { persons }
          </div>
        );
      }

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -45,7 +46,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -66,6 +71,10 @@ class App extends Component {
       )
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -87,10 +96,13 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
 
 
 
 
+// Adding and using Radium
+// Helps to use pseudo-selectors & media queries with inline styles. Can be used for both class & functional components.
+// export default Radium(App);   App is wrapped in Radium()(higher order component).
 
 
