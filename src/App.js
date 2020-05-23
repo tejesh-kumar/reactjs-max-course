5264e94bb5bda611a20e00bfcd9293a97d5a0bcf
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-23 19:49:47 +0530

    Adding dynamic styles in app.js We can pass a prop or attribute with any name, to the StyledButton component.
    In app.js,
        import React, { Component } from 'react';
        import styled from 'styled-components'
        import './App.css';
        import Person from './Person/Person';
    
        const StyledButton = styled.button`
        background-color: ${props => props.alt ? 'red' : 'green'};
        color: white;
        font: inherit;
        border: 1px solid blue;
        padding: 8px;
        cursor: pointer;
    
        &:hover {
            background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
            color: black;
        }
        `
    
        class App extends Component {
        state = {
            persons: [
            { id: "dfas1", name: "Tejesh", age: 25 },
            { id: "ghfdsd2", name: "Nitesh", age: 23 },
            { id: "ddewd3", name: "Madhu", age: 26 }
            ],
            otherState: 'some other value',
            showPersons: false
        }
    
        nameChangedHandler = (event, id) => {
            const personIndex = this.state.persons.findIndex(p => p.id === id);
            const person = {...this.state.persons[personIndex]};
            person.name = event.target.value;
            const persons = [...this.state.persons];
            persons[personIndex] = person;
            this.setState({ persons: persons })
        }
    
        deletePersonHandler = (personIndex) => {
            const persons = [...this.state.persons];
            persons.splice(personIndex, 1);
    
            this.setState({
            persons: persons
            })
        }
    
        togglePersonsHandler = () => {
            const doesShow = this.state.showPersons;
            this.setState({
            showPersons: !doesShow
            })
        }
    
        render() {
            let persons = null;
            if(this.state.showPersons) {
            persons = (
                <div>
                {
                    this.state.persons.map((person, index) => {
                    return <Person
                        id = {index} name={person.name}
                        age = {person.age}
                        click = {() => { this.deletePersonHandler(index)}}
                        changed = {(event) => this.nameChangedHandler(event, person.id)}
                        key = {person.id} />
                    })
                }
                </div>
            )
            }
    
            let classes = [];
            if(this.state.persons.length <= 2) {
            classes.push('red');
            }
            if(this.state.persons.length <= 1) {
            classes.push('bold');
            }
    
            return (
            <div className="App">
                <h1>Hi, from React app</h1>
                <p className={classes.join(' ')}>This is really working</p>
                <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
                { persons }
            </div>
            );
        }
        }
    
        export default App;
    
        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
    The 'alt' attribute contains the prop information for the StyledButtonComponent and is utilized inside the component to deliver the relevant styles as shown below.
    
        background-color: ${props => props.alt ? 'red' : 'green'};

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`
 
 class App extends Component {
   state = {
@@ -39,18 +54,18 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    }
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // }
 
     let persons = null;
     if(this.state.showPersons) {
@@ -69,11 +84,11 @@ class App extends Component {
         </div> 
       )
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
     }
 
     let classes = [];
@@ -88,7 +103,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, from React app</h1>
         <p className={classes.join(' ')}>This is really working</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+        {/* <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button> */}
         { persons }
       </div>
     );
