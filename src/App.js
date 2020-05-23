490dda3f2156061aa3ac251d184009f6e38f5364
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-23 15:12:06 +0530

    Setting class names dynamically. In render() in app component,   render() {     const style = {       backgroundColor: 'green',       color: 'white',       font: 'inherit',       border: '1px solid blue',       padding: '8px',       cursor: 'pointer'     }
        let persons = null;
        if(this.state.showPersons) {
          persons = (
            <div>
              {
                this.state.persons.map((person, index) => {
                  return <Person
                    id = {index} name={person.name}
                    age = {person.age}
                    click = {() => { this.deletePersonHandler(index)}}
                    changed = {(event) => this.nameChangedHandler(event, person.id)}
                    key = {person.id} />
                })
              }
            </div>
          )
    
          style.backgroundColor = 'red';
        }
    
        let classes = [];
        if(this.state.persons.length <= 2) {
          classes.push('red');
        }
        if(this.state.persons.length <= 1) {
          classes.push('bold');
        }
    
        return (
          <div className="App">
            <h1>Hi, from React app</h1>
            <p className={classes.join(' ')}>This is really working</p>
            <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
            { persons }
          </div>
        );
      }
    
    css classes added dynamically to paragraph like red, bold depending on length of persons array in each render.

--- src/App.js
+++ src/App.js
@@ -68,10 +68,18 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
+    let classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, from React app</h1>
-        <p>This is really working</p>
+        <p className={classes.join(' ')}>This is really working</p>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         { persons }
       </div>
@@ -82,9 +90,6 @@ class App extends Component {
 export default App;
 
 
-// Setting styles dynamically
-// Inline styling - disadvantages like media queries, pseudo-selectors
-// Button color green on showing list, color red on hiding list, styling dynamically.
 
 
 
