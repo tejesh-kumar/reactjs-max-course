3d0073340336990248104e21fc00bde6afee2c08
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-23 18:32:13 +0530

    Using Styled Components in Person.js We named the variable with starting capital letter 'StyledDiv' so that it can be used as valid, regular react component. StyledDiv contains the JSX returned by a react component provided by 3rd party library(styledComponents).
    In Person.js,
        import React from 'react'
        import styled from 'styled-components'
    
        const StyledDiv = styled.div`
            width: 60%;
            margin: 16px auto;
            border: 1px solid #eee;
            box-shadow: 0 2px 3px #ccc;
            padding: 16px;
            text-align: center;
    
            @media (min-width: 500px) {
                width: 450px;
            }
            `
    
        const person = (props) => {
            const style = {
                '@media (min-width: 500px)': {
                    width: '450px'
                }
            }
    
            return (
                // <div className="Person" style={style}>
                <StyledDiv>
                    <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
                    <p>{props.children}</p>
                    <input type="text" onChange={props.changed} value={props.name} />
                </StyledDiv>
                // </div>
            )
        }
    
        export default person;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -86,19 +85,17 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, from React app</h1>
-          <p className={classes.join(' ')}>This is really working</p>
-          <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          { persons }
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, from React app</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        { persons }
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
 
 
 
