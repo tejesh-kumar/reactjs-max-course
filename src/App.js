8f15582d7a759eb4b32122b46814513fbc117699
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-24 09:54:22 +0530

    Using css modules for dynamic styles & media queries Writing Styles which apply to one specific component. Some changes made to webpack development & production file sto use css modules. Importing person.css & app.css using modules object. Each class names are mapped to unique names in an array so that these styles only apply to this component.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
-import './App.css';
+
+import classes from './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`
 
 class App extends Component {
   state = {
@@ -54,20 +40,10 @@ class App extends Component {
   }
 
   render() {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color: 'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer',
-    //   ':hover': {
-    //     backgroundColor: 'lightgreen',
-    //     color: 'black'
-    //   }
-    // }
 
     let persons = null;
+    let btnClass = '';
+
     if(this.state.showPersons) {
       persons = (
         <div>
@@ -83,28 +59,23 @@ class App extends Component {
           }
         </div> 
       )
+          btnClass = classes.Red;
 
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, from React app</h1>
-        <p className={classes.join(' ')}>This is really working</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
-        {/* <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button> */}
+        <p className={assignedClasses.join(' ')}>This is really working</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         { persons }
       </div>
     );
@@ -115,5 +86,4 @@ export default App;
 
 
 
-// Using css modules.
-// Writing Styles which apply to one specific component.
+
