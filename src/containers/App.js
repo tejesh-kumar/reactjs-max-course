b62d56fff9afa838511ed9baf3389dfb57022b60
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-25 17:22:41 +0530

    Splitting the app into components props in Persons component will contain array of persons which must be transformed into array of JSX.

---
+++ src/containers/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "dfas1", name: "Tejesh", age: 25 },
+      { id: "ghfdsd2", name: "Nitesh", age: 23 },
+      { id: "ddewd3", name: "Madhu", age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+
+    this.setState({
+      persons: persons
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+
+  render() {
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = <Persons persons={this.state.persons} clicked={this.deletePersonHandler} changed={this.nameChangedHandler} />
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit showPersons={this.state.showPersons} persons={this.state.persons} clicked={this.togglePersonsHandler}/>
+        { persons }
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
