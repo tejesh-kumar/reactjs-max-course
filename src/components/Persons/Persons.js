62c494aff218f275461d3c45e631a4cf9e6004cf
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-27 12:20:21 +0530

    component update lifecycle for prop changes 1. getDerivedStateFromProps(props, state) Used to initialize the state of a component that updates, based on props that we are getting (eg:- form controls that get external properties, then if we internally want to handle user input, but initialize the state or update state based on prop changes.) Dont's: HTTP requests
    2. shouldComponentUpdate(nextProps, nextState)
    Evaluation by react whether or not to re-render the component for performance updation.
    Dont's: HTTP requests
    
    3. render()
    react constructs the virtual DOM and sees if it should update the real DOM.
    Prepare & structure JSX code
    update Child Component Props
    Now, every child component lifecycle starts if it receives new props.
    
    4. getSnapshotBeforeUpdate(prevProps, prevState)
    This lifecycle hook takes previous props & previous state as input and actually returns a snapshot object which we can freely configure.
    Used for last minute DOM operations(eg:- getting current scrolling position of the user)
    Rarely used lifecycle hook
    Dont's: HTTP requests
    
    5. componentDidUpdate()
    the hook signals that update is done, render() already has executed.
    Now, we can make HTTP requests.
    Dont's: Update state (Triggers re-render)
    
    practice
    Persons.js &Person.js converted to class based components to observe the update lifecycle of class components on prop changes in practice.
    In Persons.js,
    shouldComponentUpdate(nextProps, nextState) { return true }
    Receives the upcoming state & props, that the component receives after updation
    'true' indicates the component to be updated and 'false' if component should not be updated. Logic like comparing current props to next props and returning true or false.
    
    getSnapshotBeforeUpdate()
    this is like a data packet which can be accesssed in the componentDidUpdate() lifecycle hook. So, we can save some state before the update like 'scroll position' which can be used to update the DOM or update the scrolling of user in componentDidUpdate() once the DOM re-rendered.

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person'
 
-const persons = (props) => {
-    console.log('[Persons.js] rendering...');
+// const persons = (props) => {
+class Persons extends Component {
 
-    return props.persons.map((person, index) => {
-        return <Person 
-            id = {index} name={person.name} 
-            age = {person.age} 
-            click = {() => { props.clicked(index)}} 
-            changed = {(event) => props.changed(event, person.id)}
-            key = {person.id}/>
-    })
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;          // return unchanged state
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return { message: 'Snapshot!!!' };
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...');
+
+        return this.props.persons.map((person, index) => {
+            return <Person 
+                id = {index} name={person.name} 
+                age = {person.age} 
+                click = {() => { this.props.clicked(index)}} 
+                changed = {(event) => this.props.changed(event, person.id)}
+                key = {person.id}/>
+        })
+    }
+    
 }
 
 
-export default persons
+// export default persons
+export default Persons
+
+
 
