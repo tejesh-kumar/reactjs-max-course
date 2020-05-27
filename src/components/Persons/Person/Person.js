42bfdd09d111e96af081f7c7f1722befc241bf09
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-27 22:07:00 +0530

    Rendering the adjacent JSX elements using HOC Note:- React allows to render an array of JSX elements if each element has a 'key' prop. hoc - Higher order components hoc are wrappers used to wrap adjacent JSX elements into root element, which are not required as part of HTML(not equivalent to wrapping divs) but only satisfies requirement for React as adjacent JSX elements wrapped in single hoc wrapper.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import Aux from '../../../hoc/Auxiliary'
+
 import classes from './Person.css'
 
 // const person = (props) => {
@@ -9,11 +11,13 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
 
         return (
-            <div className={classes.Person}>
+            <Aux>
+            {/* // <div className={classes.Person}> */}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            {/* // </div> */}
+            </Aux>
         )
     }
 
