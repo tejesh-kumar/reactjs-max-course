8acf2e62746a7d94be0fc1c4c5b21627047f986b
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-27 22:14:18 +0530

    React.Fragment() Equivalent to Aux component built into React since 16.2 In person component,     class Person extends Component {
            render() {
                console.log('[Person.js] rendering...');
    
                return (
                    <Fragment>
                        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                        <p>{this.props.children}</p>
                        <input type="text" onChange={this.props.changed} value={this.props.name} />
                    </Fragment>
                )
            }
    
        }

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import Aux from '../../../hoc/Auxiliary'
 
@@ -11,13 +11,13 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
 
         return (
-            <Aux>
+            <Fragment>
             {/* // <div className={classes.Person}> */}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
             {/* // </div> */}
-            </Aux>
+            </Fragment>
         )
     }
 
