dd0dab1c963cd82fad29c597ba377e987596b2f4
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-28 13:32:27 +0530

    Custom Higher Order Components Wraps an other component & adds something to it(eg:- styling or logic). Can be used for handling errors in components that perform HTTP requests. 'WithClass' is an example HOC used in app.js.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -84,7 +85,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {
           this.setState({showCockpit: false})
         }}>Remove Cockpit</button>
@@ -92,7 +93,7 @@ class App extends Component {
         { this.state.showCockpit === true ?
         <Cockpit title={this.props.appTitle} showPersons={this.state.showPersons} personsLength={this.state.persons.length} clicked={this.togglePersonsHandler}/> : null}
         { persons }
-      </div>
+      </WithClass>
     );
   }
 }
