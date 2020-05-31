fcc6449384a0818b085da2d70f0bc95717b24147
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-31 18:13:13 +0530

    Passing unknown props from app to person component withClass returns a functional component which wraps the component(person or app) with a div with className. Here, the props passed on Person JSX component in app.js is passed as props object to the person component through withClass higher order component as shown below,     const withClass = (WrappedComponent, className) => {         return props => (             <div className={className}>                 <WrappedComponent {...props} />             </div>         )     }
    {...props} is equivalent to <WrappedComponent name="tejesh" age=25 />
    These prop properties can be used in Person component, as before.
    
    In person component,
        import React, { Component } from 'react'
    
        import Aux from '../../../hoc/Auxiliary'
        import withClass from '../../../hoc/withClass'
    
        import classes from './Person.css'
    
        class Person extends Component {
            render() {
                console.log('[Person.js] rendering...');
    
                return (
                    <Aux>
                        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                        <p>{this.props.children}</p>
                        <input type="text" onChange={this.props.changed} value={this.props.name} />
                    </Aux>
                )
            }
    
        }
    
        export default withClass(Person, classes.Person)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../../hoc/Auxiliary'
+import withClass from '../../../hoc/withClass'
 
 import classes from './Person.css'
 
@@ -11,20 +12,20 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
 
         return (
-            <Fragment>
+            <Aux>
             {/* // <div className={classes.Person}> */}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
             {/* // </div> */}
-            </Fragment>
+            </Aux>
         )
     }
 
 }
 
 // export default person;
-export default Person
+export default withClass(Person, classes.Person)
 
 
 
