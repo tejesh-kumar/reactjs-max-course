3b4f2bed27109138795f8fb992feeefab0c67a15
tejesh <tejeshmuralikrishna@gmail.com>
2020-05-31 18:56:53 +0530

    Setting state correctly in class components To avoid using setState() in the incorrect way, when updating state depends on previous state.
    Suppose we want to count the number of keystrokes used to update names in input field 'changeCounter' & is initially set to zero & it increments on every key stroke.
    As the counter state is dependent on previous count(previous count + 1), we are using setState() synchronously. But, these setState() calls are scheduled & executed by react when it has required resources(no other higher priority task than updating state).
    In large apps, updating state this way, might lead to problems.
    example:- If previous setState() call still not executed by react but, present setState() call is scheduled, but is actually dependent on result of previous setState() call.
        nameChangedHandler = (event, id) => {
          const personIndex = this.state.persons.findIndex(p => p.id === id);
          const person = {...this.state.persons[personIndex]};
          person.name = event.target.value;
          const persons = [...this.state.persons];
          persons[personIndex] = person;
          this.setState({ persons: persons, changeCounter: this.state.changeCounter + 1 })
        }
    
    Better way to update state when updated state is dependent on previous state,
    setState() not only takes object, but also accepts functions as argument
        nameChangedHandler = (event, id) => {
          const personIndex = this.state.persons.findIndex(p => p.id === id);
          const person = {...this.state.persons[personIndex]};
          person.name = event.target.value;
          const persons = [...this.state.persons];
          persons[personIndex] = person;
          this.setState((prevState, props) => {
            return {
              persons: persons,
              changeCounter: prevState.changeCounter + 1
            }
          })
        }

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,7 +30,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -57,7 +58,12 @@ class App extends Component {
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({ persons: persons })
+    this.setState((prevState, props) => {
+      return { 
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1 
+      }
+    })
   }
 
   deletePersonHandler = (personIndex) => {
@@ -102,6 +108,7 @@ class App extends Component {
 }
 
 export default withClass(App, classes.App);
+
 
 
 
